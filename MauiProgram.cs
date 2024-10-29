db6b09a0a8baa41c9489d877c82d1494394cbeaa
ismael Massolo <ismaelmassolo@gmail.com>
2024-10-29 11:21:15 -0300

    actualizacion 2

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace rootear
 {
@@ -14,6 +15,8 @@ namespace rootear
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("materialdesignicons-webfont.ttf", "MaterialDesignIcons");
+
                 });
 
 #if DEBUG
